b5aa956a458dd5fe30974a3365b2776eef6b49da
Your Name <you@example.com>
2020-07-09 11:13:42 +0300

    fix bug, add sleep mode

--- BLE_VESC_HC12_NEOPIXEL.ino
+++ BLE_VESC_HC12_NEOPIXEL.ino
@@ -2,11 +2,10 @@
 #include <Adafruit_NeoPixel.h>
 #include <VescUart.h>
 
+//#define DEBUG 1
 #define PIN         PB9// On Trinket or Gemma, suggest changing this to 1
 #define NUMPIXELS   24
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-
 
 VescUart UART;
 
@@ -20,37 +19,40 @@ void setup() {
 
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
 
+#ifdef DEBUG
   Serial.begin(115200);
+#endif
   Serial1.begin(115200);
   Serial2.begin(115200);
 
 }
 
-
-int pooling_cnt = 0;
-const int POOLING_MAX = 50000;
-bool flag = false;
 long lastTime_COMM_GET_VALUES = 0;
 
-
 long lastBLEms = 0;
-
-long loopingMS = 0;
 bool bleRequested = false;
 
 int return_mode  = 0;
-long seconds = 1000;
-bool valueRead = false;
 
-long lastMS = 0;
+long lastMainloopMillis = 0;
+long thisMainLoopMillis = 0;
+long lastDataMillis = 0;
+bool sleepMode = false;
+
 
 // MAIN LOOP START! ========================================
 void loop() {
+  thisMainLoopMillis = millis();
 
   //  standalone mode without BLE
-    if (millis() - lastBLEms > 5000) {
-      bleRequested  = false;
-    }
+  if (thisMainLoopMillis - lastBLEms > 5000) {
+    bleRequested  = false;
+  }
+
+  // sleep mode
+  if (thisMainLoopMillis - lastDataMillis  > 10000) {
+    sleepMode = true;
+  }
 
 
   // VESC(HC12)-> BLE ============================
@@ -63,15 +65,23 @@ void loop() {
 
     return_mode = UART.getVescValues(inByte);
 
-    //    if (return_mode == 4) {
+
     if (return_mode > 0) {
+      check_brake();
+      pixels.show();
+
+#ifdef DEBUG
       Serial.print("return_mode ");
       Serial.println(return_mode);
-      Serial.println(millis() - lastTime_COMM_GET_VALUES );
-      lastTime_COMM_GET_VALUES = millis();
+      Serial.println(thisMainLoopMillis - lastTime_COMM_GET_VALUES );
+#endif
+      lastTime_COMM_GET_VALUES = thisMainLoopMillis;
 
     } else {
     }
+
+    lastDataMillis = thisMainLoopMillis;
+    sleepMode = false;
   }
 
   // BLE -> VESC(HC12) =============================
@@ -79,68 +89,128 @@ void loop() {
     volatile  uint8_t inByte = Serial2.read();
     Serial1.write(inByte);
     bleRequested = true;
-    lastBLEms = millis();
+    lastBLEms = thisMainLoopMillis;
+    lastDataMillis = thisMainLoopMillis;
+    sleepMode = false;
   }
 
+  // ms
+  spin(20);
+
+}
 
 
-  //every (    )ms =====================================
-  if (millis() - loopingMS > 100) {
-    loopingMS = millis();
+// SPIN ======================================================================================
+long currnetTime;
+
+void spin(int desiredMillis) {
+  static long lastSpinTime;
+
+  if (thisMainLoopMillis - lastSpinTime >  desiredMillis) {
+    lastSpinTime = currnetTime;
     looping_function();
   }
+
+  if (sleepMode)
+  {
+    sleep();
+  }
+
+
 }
 
-void looping_function() {
+int brakeState = 0;
+enum BRAKE_CASE  {NO_BRAKE, NEUTRAL, ACCELERATION, BRAKE_DECELERATION, BRAKE_REVERSE };
+int before_brake_status = 0;
 
-  if (millis() - lastTime_COMM_GET_VALUES > 100 && !bleRequested) {
+int ledCount = 0;
+void looping_function() {
+  if (thisMainLoopMillis - lastTime_COMM_GET_VALUES > 20 && !bleRequested) {
+    lastTime_COMM_GET_VALUES = thisMainLoopMillis;
     UART.requestVescGetValues();
+
+#ifdef DEBUG
     Serial.println("request data manually");
     Serial.println(lastTime_COMM_GET_VALUES);
-    bleRequested = false;
+#endif
 
   }
-    check_brake();
+  //  check_brake();
+#ifdef DEBUG
+  Serial.println(UART.data.rpm);
+#endif
 
 
 }
-
-bool brake = false;
-bool before_brake_status = false;
 
 
 void check_brake(void) {
-  // brake case 1 : negative
+
+  // BRAKE_DECELERATION
   if (UART.data.avgMotorCurrent < 0) {
-    Serial.println("brake: current nagative");
-    brake = true;
+    brakeState = BRAKE_DECELERATION;
   }
-  // brake case 2 : low speed with nagative
-  else if (UART.data.rpm < 0) {
-    Serial.println("brake: low reverse");
-    brake = true;
+
+  // NEUTRAL
+  else if (UART.data.avgMotorCurrent == 0 && UART.data.rpm >= 0 ) {
+    brakeState = NEUTRAL;
   }
-  else {
-    Serial.println("no brake");
-    brake = false;
+
+  // ACCELERATION
+  else if (UART.data.avgMotorCurrent > 0 && UART.data.rpm > 0 ) {
+    brakeState = ACCELERATION;
+  }
+
+  // BRAKE_REVERSE
+  else if (UART.data.rpm <= 0) {
+    brakeState = BRAKE_REVERSE;
+  }
+
+  int tempRPM = UART.data.rpm;
+  if (tempRPM > 15000) {
+    tempRPM = 15000;
+  }
+  int rpm_mapped = map(UART.data.rpm, 0, 15000, 0, 255);
+
+  if (brakeState != before_brake_status) {
+    ledCount = 0;
+  }
+
+  switch (brakeState) {
+
+    case BRAKE_DECELERATION:
+      setColor(255, 0, 0);
+      break;
+
+    case NEUTRAL:
+      if (ledCount++ < 5) {
+        setColor(255, 255, 255);
+
+      } else if (ledCount++ < 130) {
+        setColor(30, 3, 3);
+      } else {
+        ledCount = 0;
+      }
+
+      break;
+
+    case ACCELERATION:
+      setColor(rpm_mapped / 2, rpm_mapped / 2, rpm_mapped);
+      break;
+
+
+    case BRAKE_REVERSE:
+      setColor(150, 0, 0);
+      break;
+
+    default:
+      break;
+
 
   }
 
 
-  if (brake) {
-    if (brake != before_brake_status) {
-      setColor(200, 10, 10);
-      pixels.show();   // Send the updated pixel colors to the hardware.
-    }
-    before_brake_status = true;
-  }
-  else {
-    if (brake != before_brake_status) {
-      setColor(1, 10, 10);
-      pixels.show();   // Send the updated pixel colors to the hardware.
-    }
-    before_brake_status = false;
-  }
+  before_brake_status = brakeState;
 }
 
 
@@ -149,4 +219,19 @@ void setColor(int r, int g, int b) {
     pixels.setPixelColor(i, pixels.Color(r, g, b));
 
   }
+}
+
+void sleep() {
+  for (int i = 0; i < 25; i ++) {
+    setColor(i, i / 10, i / 10);
+    pixels.show();
+    delay(10);
+  }
+  for (int i = 25; i >= 0; i --) {
+    setColor(i, i / 10, i / 10);
+    delay(10);
+    pixels.show();
+  }
+  delay(3000);
+
 }
